297610dfe216c4b8627b3eb34591ff72e38c8578
Your Name <you@example.com>
2024-04-25 22:12:29 +0200

    ја а а а бићу само твоја

--- model.py
+++ model.py
@@ -1,2 +1,67 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 3, 15)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(2, 2, 30)
+        self.fc1 = nn.Linear(30 * 15 * 15, 29)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) 
+        x = F.relu(self.fc1(x))
+        return x
+
+    def train(self, trainloader):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
+
+        for epoch in range(10):
+
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                inputs, labels = data[0].to(device), data[1].to(device)
+
+                optimizer.zero_grad()
+
+                outputs = self(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+                if i % 100 == 99:    # print every 100 mini-batches
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
+                    running_loss = 0.0
+
+        print('Finished Training')
+
+    def evaluate(self, testloader):
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = self(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print(f'Tacnost mreze: {100 * correct // total} %')
+
+    def save_model(self, path):
+        torch.save(self.state_dict(), path)
+
+    def load_model(self, path):
+        self.load_state_dict(torch.load(path))
