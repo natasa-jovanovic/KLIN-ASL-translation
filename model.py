22e27bfcaddcc0418d1a86c79088379a53dd700e
natasa-jovanovic <jovanovicnatasa001@gmail.com>
2024-05-09 12:51:26 +0200

    Improved model, changed saving of the data

--- model.py
+++ model.py
@@ -6,30 +6,22 @@ import torch.optim as optim
 
 
 class Net(nn.Module):
-
-    def __init__(self):
+    def __init__(self, input_shape=[3, 5, 5], num_classes=29, conv_layers=[16, 32], fc_layers=[120, 84], strides=[5, 5]):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 29)
+        self.conv1 = nn.Conv2d(input_shape[0], conv_layers[0], 5)
+        self.conv2 = nn.Conv2d(conv_layers[0], conv_layers[1], 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.bn1 = nn.BatchNorm2d(conv_layers[0])
+        self.bn2 = nn.BatchNorm2d(conv_layers[1])
+        self.fc1 = nn.Linear(conv_layers[1] * 4 * 4, fc_layers[0])
+        self.fc2 = nn.Linear(fc_layers[0], fc_layers[1])
+        self.fc3 = nn.Linear(fc_layers[1], num_classes)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
+        x = self.pool(self.bn1(F.relu(self.conv1(x))))
+        x = self.pool(self.bn2(F.relu(self.conv2(x))))
+        x = x.view(-1, 32 * 4 * 4)
         x = F.relu(self.fc1(x))
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-    def save_model(self, path):
-        torch.save(self.state_dict(), path)
-
-    def load_model(self, path):
-        self.load_state_dict(torch.load(path))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
